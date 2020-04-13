5128d97941eb701f8b7b93caa95464c4fec5dfc4
Arita Malik <hello@coderport.com>
2020-04-13 15:41:20 +0530

    Added response fields in asciidoc

--- src/test/java/com/codelovin/springboot/restdocstestsdemo/PersonControllerTest.java
+++ src/test/java/com/codelovin/springboot/restdocstestsdemo/PersonControllerTest.java
@@ -13,25 +13,31 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
 @WebMvcTest(PersonRestController.class)
 @AutoConfigureRestDocs(outputDir = "target/snippets")
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@ExtendWith({RestDocumentationExtension.class})
 public class PersonControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc; 
     
 	@MockBean
     private PersonService personService;
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
+    	
+    	FieldDescriptor[] book = new FieldDescriptor[] {
+    			fieldWithPath("message").description("The greeting message for the user") };
+    	
         this.mockMvc.perform(get("/api/persons/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(containsString("Hello, World")))
-            .andDo(document("home"));
+            .andDo(document("home", responseFields(book)));
     }
 }
